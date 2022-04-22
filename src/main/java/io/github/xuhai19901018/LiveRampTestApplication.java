55bdcde08c3f6344d78b514dc477dab044a13b44
xuhai19901018 <937410477@qq.com>
2022-04-22 18:07:45 +0800

    Initial commit

---
+++ src/main/java/io/github/xuhai19901018/LiveRampTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.xuhai19901018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveRampTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveRampTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/xuhai19901018/LiveRampTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xuhai19901018;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveRampTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
